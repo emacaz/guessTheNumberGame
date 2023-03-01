c3f7c97b804f525856bfc8737acf4185942e54c9
Emanuel C <ecastillo76@misena.edu.co>
2023-03-01 17:15:47 -0500

    Starting the files

---
+++ index.js
@@ -0,0 +1,13 @@
+// VARIABLES
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
+guessField.focus(); // enfoca el cursor en el cuadro de texto
